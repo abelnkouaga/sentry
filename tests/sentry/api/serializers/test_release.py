4bad9d5f569c5736b714af4fbc024fd390c09b26
Katie Lundsgaard <katie@getsentry.com>
2017-01-24 16:56:07 -0800

    Try again to fix test by removing specified date from test commit

--- tests/sentry/api/serializers/test_release.py
+++ tests/sentry/api/serializers/test_release.py
@@ -80,10 +80,6 @@ class ReleaseSerializerTest(TestCase):
             organization_id=project.organization_id,
             repository_id=1,
             key='abc',
-<<<<<<< HEAD
-            date_added='2016-12-14T23:37:37.166Z',
-=======
->>>>>>> 1e99fa9... Don't specify date at all in test commit
             author=commit_author,
             message='waddap',
         )
@@ -120,7 +116,10 @@ class ReleaseSerializerTest(TestCase):
             organization_id=project.organization_id,
             repository_id=1,
             key='abc',
+<<<<<<< HEAD
             date_added='2016-12-14T23:37:37.166Z',
+=======
+>>>>>>> 668068d... Try again to fix test by removing specified date from test commit
             author=commit_author,
             message='waddap',
         )
